dfeba52ebde67b50f3b66685b795054e64ef49fc
Michał Iwanicki <michaliwanicki35@gmail.com>
2025-07-01 16:56:19 +0200

    added user list

--- patients/forms.py
+++ patients/forms.py
@@ -1,21 +1,54 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 from .models import Patient
 
 class PatientForm(forms.ModelForm):
+   
+
     class Meta:
         model = Patient
-        fields = ['pesel_encrypted', 'first_name_encrypted', 'last_name_encrypted', 'email', 'phone']
+        fields = [
+            'pesel_encrypted',
+            'first_name_encrypted',
+            'last_name_encrypted',
+            'email',
+            'phone',
+            'date_of_birth',
+        ]
         widgets = {
             'pesel_encrypted': forms.TextInput(attrs={
                 'class': 'input input-bordered w-full',
-                'placeholder': 'PESEL',
-                'hx-trigger': 'keyup changed delay:500ms',
-                'hx-get': "{% url 'patients:list' %}",
-                'hx-target': '#patient-list',
-                'hx-include': '[name=pesel_encrypted]',
+                'placeholder': 'PESEL (11 cyfr)',
+                'autocomplete': 'off',
             }),
-            'first_name_encrypted': forms.TextInput(attrs={'class': 'input input-bordered w-full', 'placeholder': 'Imię'}),
-            'last_name_encrypted': forms.TextInput(attrs={'class': 'input input-bordered w-full', 'placeholder': 'Nazwisko'}),
-            'email': forms.EmailInput(attrs={'class': 'input input-bordered w-full', 'placeholder': 'Email'}),
-            'phone': forms.TextInput(attrs={'class': 'input input-bordered w-full', 'placeholder': 'Telefon'}),
+            'first_name_encrypted': forms.TextInput(attrs={
+                'class': 'input input-bordered w-full',
+                'placeholder': 'Imię',
+                'autocomplete': 'off',
+            }),
+            'last_name_encrypted': forms.TextInput(attrs={
+                'class': 'input input-bordered w-full',
+                'placeholder': 'Nazwisko',
+                'autocomplete': 'off',
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'input input-bordered w-full',
+                'placeholder': 'Email',
+                'autocomplete': 'off',
+            }),
+            'phone': forms.TextInput(attrs={
+                'class': 'input input-bordered w-full',
+                'placeholder': 'Telefon',
+                'autocomplete': 'off',
+            }),
         }
+
+    def clean_pesel_encrypted(self):
+        pesel = self.cleaned_data.get('pesel_encrypted')
+        if pesel in (None, ''):
+            raise ValidationError("Pole PESEL jest wymagane.")
+        if not pesel.isdigit() or len(pesel) != 11:
+            raise ValidationError("PESEL musi składać się z 11 cyfr.")
+        return pesel
+
--- patients/views.py
+++ patients/views.py
@@ -1,51 +1,66 @@
 from django.views.generic import ListView, CreateView
-from django.http import HttpResponse
-from django.template.loader import render_to_string
+from django.urls import reverse_lazy
+from django.contrib import messages
 from .models import Patient
 from .forms import PatientForm
-
 class PatientListView(ListView):
     model = Patient
-    context_object_name = 'patients'
     template_name = 'patients/patient_list.html'
+    context_object_name = 'patients'
     paginate_by = 25
+
+    # definiujemy pola sortowania
+    SORT_FIELDS = {
+        'name': ['first_name_encrypted', 'last_name_encrypted'],
+        'pesel': ['pesel_encrypted'],
+        'dob': ['date_of_birth'],
+        'age': ['date_of_birth'],  # starsze daty -> większy wiek
+        'email': ['email'],
+        'phone': ['phone'],
+    }
 
     def get_queryset(self):
         qs = super().get_queryset()
-        q = self.request.GET.get('q') or self.request.GET.get('pesel_encrypted')
+        q = self.request.GET.get('q', '').strip()
         if q:
-            # wyszukiwanie po pełnym imieniu/nazwisku lub PESEL
-            qs = Patient.objects.search_by_full_name(q) | Patient.objects.search_by_pesel(q)
+            ids = [p.id for p in qs if q in p.get_decrypted_pesel() or q.lower() in p.get_decrypted_full_name().lower()]
+            qs = qs.filter(id__in=ids)
+        # sortowanie domyślne
+        sort = self.request.GET.get('sort', '')
+        direction = ''
+        if sort.startswith('-'):
+            direction = '-'
+            sort = sort[1:]
+        if sort in self.SORT_FIELDS:
+            order = [f"{direction}{f}" for f in self.SORT_FIELDS[sort]]
+            return qs.order_by(*order)
         return qs.order_by('-created_at')
 
-    def render_to_response(self, context, **response_kwargs):
-        if self.request.htmx:
-            # gdy to żądanie HTMX, zwracamy tylko fragment z listą
-            html = render_to_string('patients/partials/patient_list.html', context, request=self.request)
-            return HttpResponse(html)
-        return super().render_to_response(context, **response_kwargs)
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['q'] = self.request.GET.get('q', '')
+        ctx['sort'] = self.request.GET.get('sort', '')
+        return ctx
 
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['q'] = self.request.GET.get('q', '')
+        ctx['sort'] = self.request.GET.get('sort', '')
+        return ctx
+    
 
 class PatientCreateView(CreateView):
     model = Patient
     form_class = PatientForm
     template_name = 'patients/patient_form.html'
+    success_url = reverse_lazy('patients:list')
 
     def form_valid(self, form):
-        obj = form.save()
-        if self.request.htmx:
-            # po udanym zapisie: odśwież listę pacjentów
-            patients = Patient.objects.order_by('-created_at')[:25]
-            html = render_to_string('patients/partials/patient_list.html', {'patients': patients}, request=self.request)
-            return HttpResponse(html, headers={
-                'HX-Trigger': 'patientListChanged'
-            })
-        return super().form_valid(form)
+        response = super().form_valid(form)
+        messages.success(self.request, 'Pacjent został dodany.')
+        return response
 
     def form_invalid(self, form):
-        if self.request.htmx:
-            return HttpResponse(
-                render_to_string('patients/partials/patient_form.html', {'form': form}, request=self.request),
-                status=400
-            )
+        # by messages nie nadpisywały alertów w szablonie, po prostu renderujemy z błędami
         return super().form_invalid(form)
